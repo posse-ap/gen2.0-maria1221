27b3bce87f5c441623f83e8e0b7734f76bc3715e
Maria Semba <moliaiqianyu@gmail.com>
2021-08-02 18:19:33 +0900

    解答ボックスが表示されるようにした

--- quizy.js
+++ quizy.js
@@ -1,65 +1,41 @@
-// const choices = document.getElementById('choice2');
 'use strict';
 
-
-
-        const img =document.getElementById('img-area');   
-
-
-    const quizImg = document.createElement('img');
-    quizImg.src = "https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png" 
-    quizImg.alt = "問いとなる地名の画像";
-    img.appendChild(quizImg);   
-    
-    let x = 0;
-    while (x <= 10) {
-        const  questions = document.getElementById('question-area');
-        const question = document.createElement('h2');
-        question.innerText = `${x + 1}. この地名はなんて読む？`;
-        questions.appendChild(question); 
-
-        const choices = document.querySelectorAll('ul > li');
-        //querySelectorAll('取得したい要素名')  取得したい要素に合致するセレクタをすべて引数として渡して使う
-        // #choices > li  choicesというidがついた直下のli要素
-    
-        let allQuiz = [ 
-            ['たかなわ', 'こうわ', 'たかわ'],
-            ['かめいど', 'かめど', 'かめと'],
-            ['こうじまち', 'かゆまち', 'おかとまち'],
-            ['おなりもん', 'おかどもん', 'ごせいもん'],
-            ['とどろき', 'たたら', 'たたりき'],
-            ['しゃくじい', 'いじい', 'せきこうい'],
-            ['ぞうしき', 'ざっしょく', 'ざっしき'],
-            ['おかちまち', 'ごしろちょう', 'みとちょう'],
-            ['ししぼね', 'ろっこつ', 'しこね'],
-            ['こぐれ', 'こばく', 'こしゃく']
-          ];
-        allQuiz.forEach(function(element){
-           ul[0].textContent = allQuiz[x][element]
-           ul[1].textContent = allQuiz[x][element]
-           ul[3].textContent = allQuiz[x][element] 
-        });
-
-// element 要素
-
-        // choices[0].innerText = allQuiz[x, 1];
-        // choices[1].innerText = allQuiz[x, 2];
-        // choices[2].innerText = allQuiz[x, 3];
-        x += 1;
-    }
- 
-// for(let i = 0; i < allQuiz.length; i++) {
-           
- //https://teratail.com/questions/308801
-
-// allQuiz[currentNum].c.forEach(choices => { //choicesにallQuizのnum番目のc要素を1つずつ渡す
-//     const li = document.createElement('li');
-//     li.textContent = choices; //生成したliタグにchoicesを代入
-//     // choices.appendChild(li); // choicesというidにliを入れる
+// let choices = document.getElementsByClassName('choice');
+// //classNameの方は返り値がHTMLCollection
+// choices = Array.from( choices );
+// //HTMLCollectionから配列にする [たかなわ, たかわ, こうわ]
+// choices.forEach(choice => {
+//     choice.classList.add("correct");
 // });
+// let correct = document.getElementsByClassName('correct');
+// console.log(choice);
 
-// for (let i = 0; i <= 10; i++) {
-//     document.getElementById('choice2').addEventListener('click', function() {
-//         document.getElementById('choice2').style.backgroundColor = 'lightblue';
-//     });
-// }
+let correct = document.getElementById("correct");
+correct.onclick = function() {
+    correct.classList.add("correct"); 
+
+    let answerBox = document.createElement('div');
+    //divタグを作ってanswerBoxというクラスを作る
+    answerBox.classList.add("answerBox");
+    let quiz = document.getElementById("quiz");
+    quiz.appendChild(answerBox);
+
+    let answerText = document.createElement('p');
+    answerText.innerText = "正解！";
+    answerText.classList.add("correctText");
+    answerBox.appendChild(answerText);
+
+    let correctText = document.createElement('p');
+    correctText.innerText = "正解は「たかなわ」です！";
+    answerBox.appendChild(correctText)
+    // answerBox.innerText="正解" divタグの中に直接書かれてしまうから問題
+
+};
+
+//pタグにすると縦並びになるのは、displayがblockだから
+
+//HTMLにアンサーボックスのクラスを設定したdivにｐタグを書いておいて、
+
+//  div.appendChild(box); エラー div is not defined
+// 
+
