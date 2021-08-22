21d6846271704b9741682c3f5ab3fa72c6c6c253
Maria Semba <moliaiqianyu@gmail.com>
2021-08-22 19:29:31 +0900

    不正解を選択した時に解答ボックスが表示されるようにした

--- quizy.js
+++ quizy.js
@@ -10,12 +10,15 @@
 // let correct = document.getElementsByClassName('correct');
 // console.log(choice);
 
+
+let answerBox = document.createElement('div');
+let correctText = document.createElement('p');
+
 let correct = document.getElementById("correct");
 correct.onclick = function() {
     correct.classList.add("correct"); 
-
-    let answerBox = document.createElement('div');
     //divタグを作ってanswerBoxというクラスを作る
+    let answerBox = document.createElement('div');
     answerBox.classList.add("answerBox");
     let quiz = document.getElementById("quiz");
     quiz.appendChild(answerBox);
@@ -25,12 +28,33 @@ correct.onclick = function() {
     answerText.classList.add("correctText");
     answerBox.appendChild(answerText);
 
-    let correctText = document.createElement('p');
     correctText.innerText = "正解は「たかなわ」です！";
     answerBox.appendChild(correctText)
     // answerBox.innerText="正解" divタグの中に直接書かれてしまうから問題
 
 };
+
+let incorrect = document.getElementById("incorrect1");
+let wrong = document.getElementById("incorrect2");
+incorrect.onclick = function() {
+    incorrect.classList.add("incorrect"); 
+    correct.classList.add("correct");
+
+    answerBox.classList.add("answerBox");
+    let quiz = document.getElementById("quiz");
+    quiz.appendChild(answerBox);
+
+    let incorrectAnswerText = document.createElement('p');
+    incorrectAnswerText.innerText = "不正解！";
+    incorrectAnswerText.classList.add("incorrectAnswerText");
+    answerBox.appendChild(incorrectAnswerText);
+
+    correctText.innerText = "正解は「たかなわ」です！";
+    answerBox.appendChild(correctText)
+    // answerBox.innerText="正解" divタグの中に直接書かれてしまうから問題
+
+};
+
 
 //pタグにすると縦並びになるのは、displayがblockだから
 
