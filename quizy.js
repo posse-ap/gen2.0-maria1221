1bced416887fbc144cb76475dcc91c4c08642f00
Maria Semba <moliaiqianyu@gmail.com>
2021-08-26 01:57:06 +0900

    選択肢のずれの修正、正解を選択するとボタンの色が変わるようにした

--- quizy.js
+++ quizy.js
@@ -14,25 +14,6 @@
 // let answerBox = document.createElement('div');
 // let correctText = document.createElement('p');
 
-// let correct = document.getElementById("correct");
-// correct.onclick = function() {
-//     correct.classList.add("correct"); 
-//     //divタグを作ってanswerBoxというクラスを作る
-//     let answerBox = document.createElement('div');
-//     answerBox.classList.add("answerBox");
-//     let quiz = document.getElementById("quiz");
-//     quiz.appendChild(answerBox);
-
-//     let answerText = document.createElement('p');
-//     answerText.innerText = "正解！";
-//     answerText.classList.add("correctText");
-//     answerBox.appendChild(answerText);
-
-//     correctText.innerText = "正解は「たかなわ」です！";
-//     answerBox.appendChild(correctText)
-//     // answerBox.innerText="正解" divタグの中に直接書かれてしまうから問題
-
-// };
 
 // let incorrect = document.getElementById("incorrect1");
 // let wrong = document.getElementById("incorrect2");
@@ -67,28 +48,35 @@
 // 設問数を変えてもJSを変えないでいいようにする
 // for文を使う
 
-let choices = ['こうわ', 'たかなわ', 'たかわ', 'かめと', 'かめいど', 'かめど', 'かゆまち', 'こうじまち', 'おかとまち', 'おかどもん', 'おなりもん', 'ごせいもん', 'たたら', 'とどろき', 'たたりき', 'せきこうい', 'しゃくじい', 'いじい', 'ざっしょく', 'ぞうしき', 'ざっしき' ,'ごしろちょう', 'おかちまち', 'みとちょう', 'ろっこつ', 'ししぼね', 'しこね', 'こばく', 'こぐれ', 'こしゃく'];
+let choices = ['こうわ', 'たかわ', 'かめと', 'かめど', 'かゆまち', 'おかとまち', 'おかどもん', 'ごせいもん', 'たたら', 'たたりき', 'せきこうい', 'いじい', 'ざっしょく', 'ざっしき' ,'ごしろちょう', 'みとちょう', 'ろっこつ', 'しこね', 'こばく', 'こしゃく'];
 let correctAnswer = {
-    1:'たかなわ',
-    2:'かめいど',
-    3:'こうじまち',
-    4:'おなりもん',
-    5:'とどろき',
-    6:'しゃくじい',
-    7:'ぞうしき',
-    8:'おかちまち',
-    9:'ししぼね',
-    10:'こぐれ',
+    0:'たかなわ',
+    1:'かめいど',
+    2:'こうじまち',
+    3:'おなりもん',
+    4:'とどろき',
+    5:'しゃくじい',
+    6:'ぞうしき',
+    7:'おかちまち',
+    8:'ししぼね',
+    9:'こぐれ',
 }
 
-for (let questions = 1; questions < 11; questions++) {
+let questionsLength = Object.keys(correctAnswer).length;
+// Object.keys(オブジェクト名).length   オブジェクトの長さを取得する
+// console.log(questionsLength);
+
+
+for (let questions = 0; questions < questionsLength; questions++) {
     let questionContent = 
     '<div class="quiz">'
     +'<div class="question"  id ="quiz">'
-    +'<h2 class="underline">' + questions + '. この地名はなんて読む？</h2>'
-    +'<img src="../quizy/' + questions + '.png" alt=問いとなる地名の画像 />'
-    +'<ul>'+'<li class="choice" id = "incorrect"' + questions + '>' + choices[questions] + '</li>' + '<li class="choice" id = "correct">' + choices[questions+1] +'</li>'
-    +'<li class="choice" id = "incorrect2">' + choices[questions+2] + '</li>'
+    +'<h2 class="underline">' + `${questions+1}` + '. この地名はなんて読む？</h2>'
+    +'<img src="../quizy/' + `${questions+1}` + '.png" alt=問いとなる地名の画像 />'
+    +'<ul>'
+    +'<li class="choice" id = "incorrect">' + choices[questions*2] + '</li>' 
+    +'<li class="choice correct_choice" id = "correct' + questions +'">' + correctAnswer[questions] +'</li>'
+    +'<li class="choice" id = "incorrect2">' + choices[questions*2+1] + '</li>'
     +'</ul>'
     +'</div>'
 
@@ -100,3 +88,32 @@ for (let questions = 1; questions < 11; questions++) {
     // position には'beforebegin''afterbegin''beforeend''afterend'の4つを指定できる
     // document.currentScript 現在処理を実行しているSCRIPT要素を取得
 }
+
+// 「絶対パス」は、インターネット上での一意のアドレスだからファイルの場所を確実に伝えることができるが、同一サーバー上のファイルを全て「絶対パス」で記述すると画像ファイルを読み込む度にドメインからIPを探し、サーバーの画像ファイルを見つけて呼び出しリクエストするため、処理が余計にかかる。「相対パス」を指定しておけば、その手間がない
+window.onload = function () {
+    for (let i = 0; i < questionsLength; i++) {
+        let correct = document.getElementsByClassName('correct_choice')[i];
+        correct.addEventListener("click", function () {
+            correct.classList.toggle("correct"); 
+            //divタグを作ってanswerBoxというクラスを作る
+            let answerBox = document.createElement('div');
+            answerBox.classList.toggle("answerBox");
+            let quiz = document.getElementById("quiz");
+            quiz.appendChild(answerBox);
+            let answerText = document.createElement('p');
+            answerText.innerText = "正解！";
+            answerText.classList.toggle("answerText");
+            answerBox.appendChild(answerText);
+            let correctText = document.createElement('p');
+            correctText.innerText = `正解は「 ${correctAnswer[i]} 」です！`;
+            answerBox.appendChild(correctText);
+            correct.style.pointerEvents = "none";
+
+            // answerBox.innerText="正解" divタグの中に直接書かれてしまうから問題
+        });
+    }
+};
+
+
+
+
