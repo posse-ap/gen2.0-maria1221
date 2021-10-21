159a91e91f02ff9906c0b50d07116d137021cda8
Maria Semba <moliaiqianyu@gmail.com>
2021-10-21 23:40:36 +0900

    エラー画面を作った

--- webapp.js
+++ webapp.js
@@ -9,3 +9,22 @@ modalCloseButton.onclick = function () {
   modalPage.style.display = "none";
   modalCover.style.display = "none";
 }
+
+
+$(function() {
+  //datepicker処理
+  $('.datepicker').datepicker({
+    // buttonImageOnly: true, // ボタンとして表示
+    showOn: "focus",  // テキストボックスをクリックでもカレンダー表示
+    showButtonPanel: true, //閉じるボタンと今日ボタンを表示
+    beforeShow: function (textbox, instance) {
+      $('.appendDatepicker').append($('#ui-datepicker-div'));
+    }
+  });
+  //カレンダーボタンをクリックしたらモーダルウィンドウを表示
+  $('#dpTextbox, .ui-datepicker-trigger').on('click', function(){
+    $('.appendDatepicker').addClass('open');
+  });
+});
+
+
