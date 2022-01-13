e6c415690d5098fed01ef725ba87dcd7041fbeec
Maria Semba <moliaiqianyu@gmail.com>
2022-01-14 01:15:47 +0900

    phpでデータベースを検索、パラメーターを取ってこれるようにし。文字化けしないようにmy.cnfを作った

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -5,6 +5,9 @@
 USE quizy_db; 
 -- 7行目以降の情報をどこに入れるかを指定してるだけ
 
+-- idをAuto incrementとprimary keyに設定
+-- idは被らないようにする。被らないものを1カラムはもつ。被らないカラム＝primary key
+-- auto increment データが作られるごとに上から番号を振る
 CREATE TABLE big_questions (
   id INT(10),
   name VARCHAR(40)
--- source/index.php
+++ source/index.php
@@ -2,8 +2,21 @@
 include('./db_connect.php');
 // データベースを検索  SELECT 表示するカラム FROM テーブル名 WHERE 検索条件
 // SELECT文を変数に格納
-$sql = "SELECT * FROM big_questions";
-// SQLステートメントを実行し、結果を変数に格納
-$stmt = $dbh->query($sql);
 
-// データベースを検索ってどういうこと？
+// filter_input URLから送信されるPOSTやGETなどの変数をフィルタリングする
+// フィルタリング 見込んでいるデータかどうかチェックする
+// input_get phpの外部から変数を受け織、オプションでフィルタリングする
+// query SQL文をデータベースに対して発行する
+// $stmt statement 宣言
+// SELECT カラム名 FROM テーブル名 WHERE 条件
+// SQL文を実行して結果を変数に格納
+$id = $_GET["id"];
+$stmt = $pdo->query("SELECT * FROM big_questions WHERE id = $id");
+// fetchAll(); SQLの結果をすべて一度に受け取る
+// PHPでデータベースの検索結果を受け取ってPHPの中のプログラムの中で利用するには、データベースから受け取ったデータを全て1つの配列に入れ、それをPHPのプログラム内で加工する方法
+$area = $stmt -> fetchAll();
+print_r($area[0]['name']) . PHP_EOL;
+
+
+
+// http://localhost/?id=2 ?以降がパラメーター。パラメーターを取ってくるのが $_GET["id"];
