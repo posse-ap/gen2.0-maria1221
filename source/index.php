f85b9b7af210097934bd31365b8f651ce3abc375
Maria Semba <moliaiqianyu@gmail.com>
2022-02-26 23:57:29 +0900

    この地名は何て読む？が10問分出るようにした

--- source/index.php
+++ source/index.php
@@ -10,39 +10,101 @@ include("./db_connect.php");
 // $stmt statement 宣言
 // SELECT カラム名 FROM テーブル名 WHERE 条件
 // SQL文を実行して結果を変数に格納
+
+// 変数がセットされているかを調べる
+// if(isset($$id = $_GET["id"])) {
+// }
+
 $id = $_GET["id"];
-$stmt = $pdo->query("SELECT * FROM big_questions WHERE id = $id");
+// query→?が使えない
+$stmt0 = $pdo->prepare("SELECT * FROM big_questions WHERE id = ?");
+// id=?→特定の値にしない→変数的な感じ
+$stmt1 = $pdo->prepare("SELECT * FROM choices");
+// $stmt2 = $pdo->prepare("SELECT questions_id,COUNT(questions_id) FROM choices GROUP BY questions_id");
+for ($i = 0; $i < 2; $i++) {
+  ${"stmt".$i}->execute([$id]);
+  ${"data" . $i} = ${"stmt" . $i}->fetchAll();
+};
+echo $data0[0]["name"];// 東京の難読地名クイズ
 // fetchAll(); SQLの結果をすべて一度に受け取る
 // PHPでデータベースの検索結果を受け取ってPHPの中のプログラムの中で利用するには、データベースから受け取ったデータを全て1つの配列に入れ、それをPHPのプログラム内で加工する方法
-$area = $stmt -> fetchAll();
+// $area = $stmt->fetchAll();
 // print_r($area[0]["name"]) . PHP_EOL;
-
-
 // http://localhost/?id=2 ?以降がパラメーター。パラメーターを取ってくるのが $_GET["id"];
 
 // URLのidが1なら東京、2なら広島
 // WHERE big_question_id で東京か広島か判別
 // $quizSetにとってきた結果の配列を入れる
 // count 配列の要素数を数える
+?>
 
-// $quizSet = $pdo ->query("SELECT * FROM big_questions WHERE big_question_id = ")
-// foreach ($quizSet as $quizSet) {
-//   $questionContent = 
-//   '<h1>ガチで'
-//   +'の人しか解けない！#';
-//   for($i = 0; $i < count($quizSet); )
-//   echo($area[i]["name"])
-//   +'</h1>'
-//   '<div class="quiz">' 
-//   +'<div class="question">' 
-//   +'<h2 class="underline">' 
-//   +`${i + 1}` 
-//   +". この地名はなんて読む？</h2>" 
-//   +'<img src="./image/' 
-//   +`${i + 1}` 
-//   +'.png" alt=問いとなる地名の画像 />' 
-//   +`<ul id='choices${i}'>` 
-//   +"</ul>" 
-//   +"</div>";
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ガチで東京の人しか解けない。#<?php print_r($area[0]["name"])?> </title>
+    <link rel="stylesheet" href="resetCSS.css">
+    <link rel="stylesheet" href="quizy.css">
+</head>
+
+
+<body>
+<div class="quiz">
+
+<?php 
+  // print_r($area[0]["name"]) . PHP_EOL;
+  // $number = $pdo->query("SELECT questions_id FROM choices GROUP BY questions_id");
+  // $numberOfSelects = $data2[0]["COUNT(questions_id)"]; 
+  // echo($numberOfSelects);//3 選択肢の数
+  // foreach ($numberOfQuestions as $value) {
+  // echo($quizSet[0]["name"]); //たかなわ
+  // $selects =<ul id='choices'></ul>
+  
+
+$choiceArray = [];
+for ($i = 0; $i < 6; $i++){
+  array_push($choiceArray, $data1[$i]["name"]);
+  // $selects =
+  // "<li class='choice'>" 
+  // . $choiceArray[$i] 
+  // . "</li>";
+  // echo($selects);
+}
+for ($i = 0; $i < 10; $i++) {
+  $h = $i + 1;
+  $main =
+    "<div class='question'>" 
+    . "<h2 class='underline'>"
+    . "{$h}. この地名はなんて読む？"
+    . "</h2>"
+    . "<img src='./image/$h.png' alt=問いとなる地名の画像/>"
+    . "<ul id='choices$i'>";
+    for ($j = 0; $j < 3; $j++){
+    $main .= "<li class='choice'>$choiceArray[$j]</li>";
+    }
+    $main .= "</ul>"
+    . "</div>";
+    echo($main);
+}
+
+
+  
+  // $quizSet_id = count($quizSet['questions_id'])   
+// foreach ($numberOfQuestions as $value) {
+  // $questionContent="ガチでの人しか解けない";
+  // + "<div class='quiz'>"
+  // + "<div class='question'>"
+  // +"<h2 class='underline'>$numberOfQuestions. この地名はなんて読む？</h2>"
+  // +"<img src='$numberOfQuestions./image/.png' alt=問いとなる地名の画像 />"
+  // +"<ul id='choices'></ul></div>";
 // }
+  // echo ($questionContent);
+?>
+</div>
+  <!-- <script src="quizy.js"></script> -->
+</body>
+
+</html> 
 
