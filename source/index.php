213d0d57e21e21d1e448d3d582b207e7070d4f4e
Maria Semba <moliaiqianyu@gmail.com>
2022-03-02 00:47:20 +0900

    idによって表示される問題数が変わるようにした

--- source/index.php
+++ source/index.php
@@ -21,15 +21,18 @@ $stmt0 = $pdo->prepare("SELECT * FROM big_questions WHERE id = ?");
 // id=?→特定の値にしない→変数的な感じ
 $stmt1 = $pdo->prepare("SELECT * FROM choices");
 $stmt2 = $pdo->prepare("SELECT count(DISTINCT questions_id) FROM choices"); 
-// $stmt3 = $pdo->prepare("SELECT count(area_id) FROM choices GROUP BY area_id"); 
-$stmt3 = $pdo->prepare("SELECT count(DISTINCT area_id) FROM choices"); 
-$stmt4 = $pdo->prepare("SELECT * FROM choices WHEN area_id = ?");
+// $stmt3 = $pdo->prepare("SELECT count(DISTINCT area_id) FROM choices"); 
+$stmt3 = $pdo->prepare("SELECT count(DISTINCT questions_id) FROM choices WHERE area_id = ?"); 
+$stmt4 = $pdo->prepare("SELECT * FROM choices WHERE area_id = ?");
+// $stmt5 = $pdo->prepare("SELECT name FROM choices WHERE questions_id = 1");
+
+// $stmt5 = $pdo->prepare("SELECT count(area_id) FROM choices WHERE area_id = ?");
+
 for ($i = 0; $i < 5; $i++) {
   ${"stmt" . $i}->execute([$id]);
   ${"data" . $i} = ${"stmt" . $i}->fetchAll();
 };
-echo $data0[0]["name"];// 東京の難読地名クイズ
-
+// 複数の結果が出るので、fetch()ではなくfetchAll()を使う→一気に取得できる
 // echo $data2[0]["count(*)"]; //6
 // echo $data1[0]["name"];//たかなわ
 // echo $data1[0]["questions_id"];//2
@@ -59,40 +62,9 @@ echo $data0[0]["name"];// 東京の難読地名クイズ
 
 <body>
 <div class="quiz">
-
+<h1><?php echo $data0[0]["name"];// 東京の難読地名クイズ?></h1>
 <?php 
-  // print_r($area[0]["name"]) . PHP_EOL;
-  // $number = $pdo->query("SELECT questions_id FROM choices GROUP BY questions_id");
-  // $numberOfSelects = $data2[0]["COUNT(questions_id)"]; 
-  // echo($numberOfSelects);//3 選択肢の数
-  // foreach ($numberOfQuestions as $value) {
-  // echo($quizSet[0]["name"]); //たかなわ
-  // $selects =<ul id='choices'></ul>
-
-// if ($data1[])
-// for ($n = 0; $n < $data2["count(DISTINCT questions_id)"]; $n++){
-
-  // echo $data3[0]["count(area_id)"];
-// for ($i = 0; $i < $data3[0]["count(area_id)"]; $i++){
-  //$data2[0]["count(*)"] questions_idが1、すなわち高輪の選択肢の数
-$choiceArray = [];
-for ($i = 0; $i < $data2[0]["count(*)"]; $i++){
-  ${"choiceArray" . $i} = [];
-    array_push(${"choiceArray" . $i}, $data1[$i]["name"]);
-//   // 問題の選択肢を追加
-//   // $data1[1]["nama"] は たかなわ
-    array_push($choiceArray, ${"choiceArray" . $i});
-}
-
-
-
-// // $choiceArray
-// }
-
-
-// echo $data2[0]["name"];//たかなわ area_idが１の時の最初の選択肢を表示
-// print_r($choiceArray); //Array ( [0] => たかなわ [1] => こうわ [2] => たかわ [3] => かめと [4] => かめど [5] => かめいど )
-for ($i = 0; $i < $data3[0]["count(DISTINCT area_id)"]; $i++) {
+for ($i = 0; $i < $data3[0]["count(DISTINCT questions_id)"]; $i++) {
   $h = $i + 1;
   $main =
     "<div class='question'>" 
@@ -101,33 +73,24 @@ for ($i = 0; $i < $data3[0]["count(DISTINCT area_id)"]; $i++) {
     . "</h2>"
     . "<img src='./image/$h.png' alt=問いとなる地名の画像/>"
     . "<ul id='choices'>";
-  // $data2[0]["DISTINCT questions_id"] 設問の数
-    // for ($n = 1; $n <= $data2[0]["DISTINCT questions_id"]; $n++) {
-    foreach(${"choiceArray" . $i} as $choiceList){
-      $main .= "<li class='choice'>$choiceList[$i]</li>";
+  // $data2[0]["count(DISTINCT questions_id)"] 3 設問の数
+  // 問題の選択肢を追加
+  // $choice = $pdo->prepare("SELECT name FROM choices WHERE questions_id = $i+1");
+  for ($k = 0; $k <= $data3[0]["count(DISTINCT questions_id)"]; $k++) {
+    ${"choice" . $k} = $pdo->prepare("SELECT name FROM choices WHERE questions_id = $k+1");
+    ${"choice" . $k}->execute([$id]);
+    ${"choiceList" . $k} = ${"choice" . $k}->fetchAll();
     }
+
+  for ($j = 0; $j <  $data2[0]["count(DISTINCT questions_id)"]; $j++){
+    $main .= "<li class='choice'>" 
+    . ${"choiceList" . $i}[$j]["name"]
+    . "</li>";
+  }
     $main .= "</ul>"
     . "</div>";
     echo($main);
-}
-
-// for ($n = 0; $n < $data2[0]["DISTINCT questions_id"]; $n++) {
-//   $choiceList = $pdo->prepare("SELECT name FROM choices WHERE questions_id = $n");
-//   // 一週目は、たかなわ、こうわ、たかわ
-//   echo $choiceList;
-//   // $main .= "<li class='choice'>$choiceList[0]</li>";
-// }
-  
-  // $quizSet_id = count($quizSet['questions_id'])   
-// foreach ($numberOfQuestions as $value) {
-  // $questionContent="ガチでの人しか解けない";
-  // + "<div class='quiz'>"
-  // + "<div class='question'>"
-  // +"<h2 class='underline'>$numberOfQuestions. この地名はなんて読む？</h2>"
-  // +"<img src='$numberOfQuestions./image/.png' alt=問いとなる地名の画像 />"
-  // +"<ul id='choices'></ul></div>";
-// }
-  // echo ($questionContent);
+} 
 ?>
 </div>
   <!-- <script src="quizy.js"></script> -->
